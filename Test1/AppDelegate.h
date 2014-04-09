41273ea79ac83c631d868a10bd48cac5f1d74665
Sunny <chenbin@Sunny-s-iMac.local>
2014-04-09 20:42:56 +0800

    1111

---
+++ Test1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test1
+//
+//  Created by Sunny on 14-4-9.
+//  Copyright (c) 2014年 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
